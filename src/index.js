a2f6872c46796dc943bcca3fe22df95d4cb6861a
Qoder2016 <1621532838@qq.com>
2018-05-24 16:27:29 +0800

    feat: 完成插件简单实现

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
 'use strict';
 
-class DepsPlugin {
+const fs = require('fs');
+
+class DepsWebpackPlugin {
   constructor(options) {
     this.options = options;
+    this.currentPath = process.cwd();
   };
 
   apply(compiler) {
-    compiler.plugin('done', function(stats) {
-      console.log(stats);
+    compiler.plugin('done', (stats) => {
+      const pkg = require(`${this.currentPath}/package.json`);
+      const pkgDeps = pkg.dependencies;
+      const keys = Object.keys(pkgDeps);
+      const nodeModules = `${this.currentPath}/node_modules`;
+      const result = {};
+
+      keys.forEach((name) => {
+        const modulePkg = require(`${nodeModules}/${name}/package.json`);
+        const version = modulePkg.version;
+
+        result[name] = version;
+      });
+
+      const compilation = stats.compilation;
+      const outputOptions = compilation.outputOptions;
+      const outputPath = outputOptions.path;
+
+      fs.writeFile(`${outputPath}/deps.json`, JSON.stringify(result, null, 4), null, (err) => {
+        if (err) {
+          // todo
+          console.log(err);          
+        }
+      });
     });
 
-    compiler.plugin('failed', function(err) {
-
+    compiler.plugin('failed', (err) => {
+      console.error(err);
     })
   }
 };
 
-module.exports = DepsPlugin;
+module.exports = DepsWebpackPlugin;
